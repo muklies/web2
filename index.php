930eeae888c059e1bb7047f50a1229f0e5ee13ae
muklies <muklies@gmail.com>
2023-04-28 17:00:48 +0700

    Upload pertama

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo "Data Mahasiswa"
+<label>Nama</label> <br>
+input name="Nama" type="text" placeholder ="Masukan nama" 
+<label>Alamat</label> <br>
+input alamat="alamat" type="text" placeholder="Masukan alamat"   
+?>
